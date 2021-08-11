fe6d3ff2871d5e243ce56071928bb8519c0e2d3a
johndward01 <johndward01@gmail.com>
2021-08-11 12:20:40 -0500

    main init

--- Arrays-and-Lists_Exercise-01-Answers/Program.cs
+++ Arrays-and-Lists_Exercise-01-Answers/Program.cs
@@ -6,7 +6,60 @@ namespace Arrays_and_Lists_Exercise_01_Answers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Use the methods you create below to print out the contents
+
+            // TODO: Declare and initialize a List<int> using Explicit typing
+
+            // TODO: Declare and initialize a List<int> using Implicit typing
+
+            // TODO: Declare and initialize an empty list of integers called myIntegerList
+
+            // TODO: Add 3 ints to the list you just created using the Add() function
+
+            // TODO: Declare and initialize an empty array of doubles called myDoublesArray with a size of 5
+
+            // TODO: Add 5 doubles to the array you just created using bracket notation
+
+            // TODO: Declare and initialize an int array using ALL 5 of the different ways you are allowed to
+            // Use the methods you created below to print out the contents
+
+            // TODO: Declare and initialize a string array using initializer syntax
+            // Use the method you created below to print out the contents
+
+            // TODO: Declare and initialize a char array using initializer syntax
+            // Use the method you created below to print out the contents
+
+            // TODO: Declare and initialize a bool array using initializer syntax
+            // Use the method you created below to print out the contents
+
+            // TODO: Declare and initialize a list of strings using initializer syntax
+            // Use the method you created below to print out the contents
+
+            // TODO: Declare and initialize a list of doubles using initializer syntax
+            // Use the method you created below to print out the contents
+
+            // TODO: Declare and initialize a list of floats using initializer syntax
+            // Use the method you created below to print out the contents
+
+            // TODO: Declare and initialize a list of chars using initializer syntax
+            // Use the method you created below to print out the contents
+
         }
+
+        // TODO: Create a method that accepts a list of ints, removes the first item from the list, and then prints the entire list to the console
+
+        // TODO: Create an overload of the above method that accepts a list of strings, removes the first item from the list, and then prints the entire list to the console
+
+        // TODO: Create an overload of the above method that accepts a list of strings, removes the first item from the list, and then prints the entire list to the console
+
+        // TODO: Create an overload of the above method that accepts a list of strings, removes the first item from the list, and then prints the entire list to the console
+
+        // TODO: Create a method that accepts an array of ints, removes the first item from the array, and then prints the entire array to the console
+
+        // TODO: Create an overload of the above method that accepts a array of strings, removes the first item from the array, and then prints the entire array to the console
+
+        // TODO: Create an overload of the above method that accepts a array of chars, removes the first item from the array, and then prints the entire array to the console
+
+        // TODO: Create an overload of the above method that accepts a array of doubles, removes the first item from the array, and then prints the entire array to the console
     }
 }
